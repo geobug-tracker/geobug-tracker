6ec56416a96d2decd5be9984835aa37c3411bd0b
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-13 12:32:23 -0700

    swapped out express for GQL

---
+++ server/typeDefs.js
@@ -0,0 +1,13 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+module.exports = typeDefs;

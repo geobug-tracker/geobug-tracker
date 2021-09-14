074f7c87209b1b2f6b093ce77ec48041ca007fb2
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-14 13:06:28 -0700

    removed extra dummy data

--- server/data.js
+++ server/data.js
@@ -6,39 +6,7 @@ const bugs = [
   {
     title: 'The backend is not working.',
     description: 'This is another description.',
-  },
-  {
-    title: 'The frontend is not working. This is a longer title. Wow holy how this long tisdfs idfosdf iosdfio sdfio sdoiffsd io',
-    description: 'This is a description.',
-  },
-  {
-    title: 'The backend is not working.',
-    description: 'This is another description.',
-  },
-  {
-    title: 'The frontend is not working. This is a longer title. Wow holy how this long tisdfs idfosdf iosdfio sdfio sdoiffsd io',
-    description: 'This is a description.',
-  },
-  {
-    title: 'The backend is not working.',
-    description: 'This is another description.',
-  },
-  {
-    title: 'The frontend is not working. This is a longer title. Wow holy how this long tisdfs idfosdf iosdfio sdfio sdoiffsd io',
-    description: 'This is a description.',
-  },
-  {
-    title: 'The backend is not working.',
-    description: 'This is another description.',
-  },
-  {
-    title: 'The frontend is not working. This is a longer title. Wow holy how this long tisdfs idfosdf iosdfio sdfio sdoiffsd io',
-    description: 'This is a description.',
-  },
-  {
-    title: 'The backend is not working.',
-    description: 'This is another description.',
-  },
+  }
 ];
 
 module.exports = bugs;

e5a279e63b1a90f8138458b2086bcdd8c647432e
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-14 08:34:25 -0700

    Added postgres with pool.
    The application will attempt to connect to postgres on your local
    machine in development mode. Make sure you have a database running. It's
    going to pick up the connection settings specified in your
    .env.development file.

---
+++ server/db/pool.js
@@ -0,0 +1,30 @@
+const { Pool, Client } = require('pg');
+
+async function connect() {
+  const config = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.DATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+  };
+  try {
+    /* Test connnection */
+    const client = new Client(config);
+    await client.connect();
+    client.end();
+
+    /* Create and export pool */
+    const pool = new Pool(config);
+    console.log(`🏊 Pool connected!`);
+
+    return pool;
+  } catch (err) {
+    console.log(
+      `❌ Database could not connect.\n Do you have a database on ${process.env.PGHOST}:${process.env.PGPORT} where the user "${process.env.PGUSER}" has access?\n Check your enviornment setup files.\n`
+    );
+    throw err;
+  }
+}
+
+module.exports = connect;

ccdadc2dcc575c1233406bc30e8454e3461bd00b
Gal Horovits <galhorovits@Gals-MBP.lan>
2021-09-14 20:17:11 -0400

    added tables to db, got getAllBugs function working

--- server/db/setup.sql
+++ server/db/setup.sql
@@ -1,29 +1,36 @@
-CREATE TABLE bug (
-    id integer NOT NULL PRIMARY KEY,
-    title varchar(100),
-    userId integer REFERENCES users(id),
-    description text,
-    priority varchar(100),
-    status varchar(100),
-    linkRepo text,
-    product varchar(100)
-);
 
-CREATE TABLE user (
-    id integer NOT NULL PRIMARY KEY,
-    username varchar(100),
-    firstName varchar(100),
-    lastName varchar(100),
-    password varchar(100),
-    email varchar(100),
-    teamId integer REFERENCES team(id),
-);
 
 CREATE TABLE team (
     id integer NOT NULL PRIMARY KEY,
     name varchar(100),
     company varchar(100)
 );
+
+CREATE TABLE users (
+    id integer NOT NULL PRIMARY KEY,
+    teamId integer,
+    username varchar(100),
+    firstName varchar(100),
+    lastName varchar(100),
+    password varchar(100),
+    email varchar(100),
+    FOREIGN KEY (teamId)
+     REFERENCES team(id)
+);
+
+CREATE TABLE bug (
+    id integer NOT NULL PRIMARY KEY,
+    title varchar(100),
+    userId integer,
+    description text,
+    priority varchar(100),
+    status varchar(100),
+    linkRepo text,
+    product varchar(100),
+    FOREIGN KEY (userId)
+     REFERENCES users(id)
+);
+
 
 -- Give me all the bugs associated with a particular user id
 -- select *

dc30eba58a79d9c74f67c8d0612f14863464d6d4
Adam Goodman <adamrgoodman1@gmail.com>
2021-09-14 21:38:38 -0400

    drag and drop core functionallity completed

--- client/components/Board/Board.jsx
+++ client/components/Board/Board.jsx
@@ -1,57 +1,41 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import Bug from "../Bug/Bug";
-
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import Column from "./Column";
 import "./Board.scss";
 
 const Board = () => {
-
   const GET_ALL_BUGS = gql`
     query {
-        bugs {
-            title
-            description
-        }
+      bugs {
+        title
+        description
+      }
     }
-`
-  const { loading, error, data } = useQuery(GET_ALL_BUGS);
+  `;
+  const { loading, error, data, refetch } = useQuery(GET_ALL_BUGS);
 
-  console.log(data);
-  if(loading) return <p>Loading...</p>
-  if(error) return <p>An error occured!</p>
-  
-  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>An error occured!</p>;
+
   return (
-    <div className='board'>
-      <div className='fields'>
-        <h4 className='title'>New</h4>
-        <div className='new column'>
-          {data.bugs.map(bug => <Bug data={bug} />)}
-        </div>
+    <div className="board">
+      <div className="fields">
+        <Column data={data} columnName={"new"} title={"New"} />
       </div>
-      <div className='fields'>
-        <h4 className='title'>In-Progress</h4>
-        <div className='inprogress column'>
-          {/* Add filter for in-progress to the map */}
-        {data.bugs.map(bug => <Bug data={bug} />)}
-        </div>
+      <div className="fields">
+        <Column data={data} columnName={"todo"} title={"To Do"} />
       </div>
-      <div className='fields'>
-        <h4 className='title'>Review</h4>
-        <div className='review column'>
-          {/* Add filter for review to the map */}
-        {data.bugs.map(bug => <Bug data={bug} />)}
-        </div>
+      <div className="fields">
+        <Column data={data} columnName={"inProgress"} title={"In-Progress"} />
       </div>
-      <div className='fields'>
-        <h4 className='title'>Done</h4>
-        <div className='done column'>
-          {/* Add filter for done to the map */}
-        {data.bugs.map(bug => <Bug data={bug} />)} 
-        </div>
+      <div className="fields">
+        <Column data={data} columnName={"review"} title={"Under Review"} />
+      </div>
+      <div className="fields">
+        <Column data={data} columnName={"done"} title={"Completed"} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Board;
